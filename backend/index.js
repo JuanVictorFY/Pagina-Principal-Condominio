928d1698384b06ea5c392c078f1609388183c631
Juan Victor <figyuptonj99@gmail.com>
2026-05-05 23:44:33 -0500

    chore(backend): reubicar index.js a la carpeta backend y actualizar dependencias

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
 
 // Cargar variables de entorno desde el archivo .env
 dotenv.config();
@@ -11,6 +12,14 @@ const PORT = process.env.PORT || 3000;
 // Middlewares principales
 app.use(cors()); // Permite peticiones desde el frontend (React)
 app.use(express.json()); // Permite a Express entender JSON en las peticiones (req.body)
+
+// --- RUTAS DE LA API ---
+app.use('/api/auth', authRoutes);
+
+// Ruta raíz (Mensaje de bienvenida para no ver "Cannot GET /")
+app.get('/', (req, res) => {
+  res.send('🏢 Bienvenido a la API del Backend de Domus');
+});
 
 // Ruta de prueba (Health Check)
 app.get('/api/health', (req, res) => {
